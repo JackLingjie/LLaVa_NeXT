fa0902544b5b5a721d81fe47325e5d979f20a18d
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-06 04:10:50 +0000

    torch run

--- scripts/train/distribute_train/torch_tesh.sh
+++ scripts/train/distribute_train/torch_tesh.sh
@@ -11,15 +11,15 @@ for ((RANK=1; RANK<NNODES; RANK++)); do
     NODE="node-$RANK"  
     echo "Starting training on $NODE with rank $RANK"  
 
-    # 其他 Rank 节点通过 SSH 后台运行
+    # 其他 Rank 节点通过 SSH 后台运行，并使用 nohup 确保不会阻塞
     ssh $NODE bash << EOF &
-    cd /tmp/LLaVa_NeXT  
-    bash scripts/train/distribute_train/finetune_siglip_qwen2_mid_stage_multinode_base.sh \
+    cd /tmp/LLaVa_NeXT
+    nohup bash scripts/train/distribute_train/finetune_siglip_qwen2_mid_stage_multinode_base.sh \
         --nproc_per_node "${NUM_GPUS}" \
         --nnodes "${NNODES}" \
         --node_rank "${RANK}" \
         --master_addr "${ADDR}" \
-        --master_port "${PORT}"  
+        --master_port "${PORT}" > train_rank_${RANK}.log 2>&1 &
 EOF
 done
 
